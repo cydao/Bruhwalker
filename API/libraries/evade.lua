f24ec8fb6f791e049b15a46ff6d3799b74902cac
solsticehub <107598962+solsticehub@users.noreply.github.com>
2023-03-28 14:46:23 +0200

    Added evade stuffs

--- API/libraries/evade.lua
+++ API/libraries/evade.lua
@@ -1,6 +1,18 @@
 ---@class evade
 local evade = {}
 
+---@return nil
+function evade:disable_evade()
+end
+
+---@return nil
+function evade:enable_evade()
+end
+
+---@return boolean
+function evade:is_authorised()
+end
+
 ---@return boolean
 function evade:is_evading()
 end
@@ -9,12 +21,48 @@ end
 function evade:is_evade_disabled()
 end
 
+---@param pos vec3
+---@return boolean
+function evade:is_dangerous(pos)
+end
+
+---@param start vec3
+---@param end vec3
+---@param speed number
+---@param delay number
+---@return boolean
+function evade:is_dangerous_path(start, end, speed, delay)
+end
+
+---@param skillshots table
 ---@return nil
-function evade:enable_evade()
+function evade:on_impossible_dodge(skillshots)
+end
+
+---@param skillshots table
+---@return nil
+function evade:on_low_chance_of_dodge(skillshots)
 end
 
 ---@return nil
-function evade:disable_evade()
+function evade:recalculate_path()
+end
+
+---@return vec3|nil
+function evade:safe_pos()
+end
+
+---@return table
+function evade:skillshots()
+end
+
+---@return table
+function evade:supported()
+end
+
+---@param pos vec3
+---@return number
+function evade:time_to_hit(pos)
 end
 
 ---@type evade
